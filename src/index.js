fb45ab0a15c955eb127a3906c135fdea42b23086
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-07-08 01:36:56 -0500

    handleClick is not a function fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => this.props.onClick({ value: 'X' })}>
+        onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -20,6 +20,11 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     }
+  }
+  handleClick(i){
+    const squares =this.state.squares.slice();
+    squares[i]='X';
+    this.setState({squares:squares});
   }
   renderSquare(i) {
     return (<Square
